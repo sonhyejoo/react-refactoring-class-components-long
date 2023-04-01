84bb09a0aba804c4f383705761038c1edd254356
sonhyejoo <seongwoo.cho@gmail.com>
2023-04-01 18:07:24 -0400

    clocktoggle refactored, clock wip

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,47 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 
-export class ClockToggle extends React.Component {
-  render () {
+export function ClockToggle({ toggleClock }) {
+  return (
+    <button type="button" className="clock-toggle" onClick={toggleClock}>
+      Toggle Clock
+    </button>
+  );
+}
+
+function Clock (props) {
+  const [time, setTime] = useState(new Date())
+  useEffect(() => {
+    setTime(new Date())
+    let interval = setInterval(time, 1000)
     return (
-      <button 
-        type="button"
-        className="clock-toggle" 
-        onClick={this.props.toggleClock}
-      >
-        Toggle Clock
-      </button>
+      console.log('Clearing Clock interval!')
+      clearInterval(interval)
     )
-  }
-} 
+  }, [])
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date(),
-    };
-  }
-  
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
-  
   componentWillUnmount() {
-    console.log("Clearing Clock interval!")
+    console.log("Clearing Clock interval!");
     clearInterval(this.interval);
   }
-  
+
   tick = () => {
-    this.setState({ time: new Date() });
-  }
+    setTime(new Date());
+  };
 
   render() {
     let hours = this.state.time.getHours();
     let minutes = this.state.time.getMinutes();
     let seconds = this.state.time.getSeconds();
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
     const timezone = this.state.time
       .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
@@ -51,22 +44,14 @@ class Clock extends React.Component {
     return (
       <section className="clock-section">
         <h1>Clock</h1>
-        <div className='clock'>
+        <div className="clock">
           <p className="time">
-            <span>
-              Time:
-            </span>
-            <span>
-              {`${hours}:${minutes}:${seconds} ${timezone}`}
-            </span>
+            <span>Time:</span>
+            <span>{`${hours}:${minutes}:${seconds} ${timezone}`}</span>
           </p>
           <p className="date">
-            <span>
-              Date: 
-            </span>
-            <span>
-              {this.state.time.toDateString()}
-            </span>
+            <span>Date:</span>
+            <span>{this.state.time.toDateString()}</span>
           </p>
         </div>
       </section>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { toQueryString } from '../utils';
+import React from "react";
+import { toQueryString } from "../utils";
 
 class Weather extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        weather: null
-      };
-    }
-    
-    componentDidMount() {
-      navigator.geolocation?.getCurrentPosition(
-        this.pollWeather,
-        (err) => console.log(err),
-        { timeout: 10000 }
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null,
+    };
+  }
 
-    pollWeather = async (location) => {
-      let url = 'http://api.openweathermap.org/data/2.5/weather?';
+  componentDidMount() {
+    navigator.geolocation?.getCurrentPosition(
+      this.pollWeather,
+      (err) => console.log(err),
+      { timeout: 10000 }
+    );
+  }
 
-      /* Remember that it's unsafe to expose your API key. (Note that pushing
+  pollWeather = async (location) => {
+    let url = "http://api.openweathermap.org/data/2.5/weather?";
+
+    /* Remember that it's unsafe to expose your API key. (Note that pushing
       files that include your key to Github will expose your key!) In
       production, you would definitely save your key in an environment variable,
       so do that here. Since this project runs in your local environment
@@ -29,30 +29,29 @@ class Weather extends React.Component {
       "process.env.<variable_name>". Make sure to .gitignore your .env file!
       Also remember to restart your server (i.e., re-run "npm start") whenever
       you change your .env file. */
-      const apiKey = '???';
+    const apiKey = "process.env.REACT_APP_WEATHER_API";
 
-      const params = {
-        lat: location.coords.latitude,
-        lon: location.coords.longitude,
-        appid: apiKey
-      };
-      
-      url += toQueryString(params);
+    const params = {
+      lat: location.coords.latitude,
+      lon: location.coords.longitude,
+      appid: apiKey,
+    };
 
-      const res = await fetch(url);
-      if (res.ok) {
-        const weather = await res.json();
-        this.setState({ weather });
-      }
-      else {
-        alert ("Check Weather API key!")
-      }
+    url += toQueryString(params);
+
+    const res = await fetch(url);
+    if (res.ok) {
+      const weather = await res.json();
+      this.setState({ weather });
+    } else {
+      alert("Check Weather API key!");
     }
+  };
 
   render() {
     const weather = this.state.weather;
-    let content = <div className='loading'>loading weather...</div>;
-    
+    let content = <div className="loading">loading weather...</div>;
+
     if (weather) {
       const temp = (weather.main.temp - 273.15) * 1.8 + 32;
       content = (
@@ -61,21 +60,18 @@ class Weather extends React.Component {
           <p>{temp.toFixed(1)} degrees</p>
         </div>
       );
-    }
-    else {
+    } else {
       content = (
         <div>
-          Weather is currently unavailable. (Are Location Services enabled?) 
+          Weather is currently unavailable. (Are Location Services enabled?)
         </div>
-      )
+      );
     }
 
     return (
       <section className="weather-section">
         <h1>Weather</h1>
-        <div className='weather'>
-          {content}
-        </div>
+        <div className="weather">{content}</div>
       </section>
     );
   }
